8b9095c6b7cbc653b43f3f5707d4e73d70ab97fa
Aishwary Tiwari <aishwary.tiwari@trigyn.com>
2022-01-07 21:51:43 +0530

    till forms

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,60 @@
+import {Recipe} from "./recipe.model";
+import {Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>()
+
+  private recipes: Recipe[] = [
+    new Recipe('Tasty Burger',
+      'A super tasty burger loaded with cheese',
+      'https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_960_720.jpg',
+      [
+        new Ingredient('Potato', 2),
+        new Ingredient('French Fried', 20)
+      ]),
+    new Recipe('Farmhouse Pizza',
+      'A super tasty Pizza loaded with vegies and double cheese',
+      'https://cdn.pixabay.com/photo/2017/12/10/14/47/pizza-3010062_960_720.jpg',
+      [
+        new Ingredient('Pizza Sauce', 1),
+        new Ingredient('Vegetables', 5),
+        new Ingredient('Cheese', 5),
+      ]),
+  ];
+
+  constructor(private slService: ShoppingListService) {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
